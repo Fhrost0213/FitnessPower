1017116da754937fc5966e61ac5b1bf09601843a
Fhrost0213 <bryan.vieregge@gmail.com>
2018-03-23 10:50:31 -0500

    Adding models for exercise types

---
+++ FitnessPower/FitnessPower/Models/Exercises/Run.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FitnessPower.Models.Exercises
+{
+    public class Run : Exercise
+    {
+        private float _distance;
+        private DateTime _time;
+
+        public float Distance
+        {
+            get { return _distance; }
+        }
+
+        public DateTime Time
+        {
+            get { return _time; }
+        }
+
+        public Run(float distance, DateTime time)
+        {
+            _distance = distance;
+            _time = time;
+        }
+    }
+}
---
+++ FitnessPower/FitnessPower/Models/Exercises/Squat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FitnessPower.Models.Exercises
+{
+    public class Squat : Exercise
+    {
+        private Tuple<int, int> _maxRepsAndWeight;
+
+        public Tuple<int, int> MaxRepsAndWeight
+        {
+            get { return _maxRepsAndWeight; }
+        }
+
+        public Squat(int maxReps, int maxWeight)
+        {
+            _maxRepsAndWeight = new Tuple<int, int>(maxReps, maxWeight);
+        }
+    }
+}
